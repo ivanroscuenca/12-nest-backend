1966507354a7d1996938e8c7c8dca3817e59beb8
ivanros <ivanros@protonmail.com>
2023-09-28 17:16:43 +0200

    video339

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema()
+export class User {
+  @Prop({ unique: true, required: true })
+  email: string;
+  @Prop({ required: true })
+  name: string;
+  @Prop({ minlength: 6, required: true })
+  password: string;
+  @Prop({ default: true })
+  isActive: boolean;
+  @Prop({ type: [String], default: ['user'] })
+  roles: string[];
+}
+
+//necesario exportarlo a la BBDD
+export const UserSchema = SchemaFactory.createForClass(User);
